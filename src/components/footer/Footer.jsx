4aaa1c1b2e81f13224a742adcdaf057b82c951a6
Dmitry-Alekseev01 <145341589+Dmitry-Alekseev01@users.noreply.github.com>
2024-07-13 20:52:59 +0300

    design
    Изменил немного футер

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -5,19 +5,13 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div className="footer">
-      <Link to="/about">About us</Link>
-      <div>
-        <a href="https://t.me/overmindv">Alex Sverhrazum</a>
-      </div>
-      <div>
-        <a href="https://t.me/vyshkochka">Leo Vyshkochka</a>
-      </div>
-      <div>
-        <a href="https://t.me/Mister_V_1">Dima Dezign</a>
-      </div>
-      <div>
-        <a href="https://t.me/amirovagil">Agile Macho</a>
+    <div>
+      <Link className="footer2">About us</Link>
+      <div className="footer">
+          <a href="https://t.me/overmindv">Alex Sverhrazum</a>
+          <a href="https://t.me/vyshkochka">Leo Vyshkochka</a>
+          <a href="https://t.me/Mister_V_1">Dima Dezign</a>
+          <a href="https://t.me/amirovagil">Agile Macho</a>
       </div>
     </div>
   );
